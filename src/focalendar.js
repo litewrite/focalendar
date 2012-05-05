529fda38cc43c1a9a40a8e8b72f1ed98fe3c38d0
Tobias Preuss <tobias.preuss@googlemail.com>
2012-05-05 17:29:34 +0200

    Prepared basic data structure.

---
+++ src/focalendar.js
@@ -0,0 +1,177 @@
+$(function(){
+
+  var Event = Backbone.Model.extend({
+
+      defaults: function() {
+        return {
+          summary: "No event summary",
+          dtstart: "No start date",
+          dtend: "No end date",
+          order: Event.nextOrder(),
+        };
+      },
+
+      initialize: function() {
+        if (!this.get("summary")) {
+          this.set({"summary": this.defaults.summary});
+        }
+        if (!this.get("dtstart")) {
+          this.set({"dtstart": this.defaults.dtstart});
+        }
+        if (!this.get("dtend")) {
+          this.set({"dtend": this.defaults.dtend});
+        }
+      },
+
+      clear: function() {
+        this.destroy();
+      }
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+    var EventList = Backbone.Collection.extend({
+
+      model: Event,
+
+      localStorage: new Store("events-backbone"),
+
+      nextOrder: function() {
+        if (!this.length) return 1;
+        return this.last().get('order') + 1;
+      },
+
+      comparator: function(event) {
+        return event.get('order');
+      }
+
+    });
+
+    var Events = new EventList;
+
+
+
+
+
+
+
+
+
+    var EventView = Backbone.View.extend({
+
+        tagName:  "li",
+
+        template: _.template($('#item-template').html()),
+
+        events: {
+          "dblclick .view"  : "edit",
+          "click a.destroy" : "clear",
+          "keypress .edit"  : "updateOnEnter",
+          "blur .edit"      : "close"
+        },
+
+        initialize: function() {
+          this.model.bind('change', this.render, this);
+          this.model.bind('destroy', this.remove, this);
+        },
+
+        render: function() {
+          this.$el.html(this.template(this.model.toJSON()));
+          this.input = this.$('.edit');
+          return this;
+        },
+
+        edit: function() {
+          this.$el.addClass("editing");
+          this.input.focus();
+        },
+
+        close: function() {
+          var value = this.input.val();
+          if (!value) this.clear();
+          this.model.save({title: value});
+          this.$el.removeClass("editing");
+        },
+
+        updateOnEnter: function(e) {
+          if (e.keyCode == 13) this.close();
+        },
+
+        clear: function() {
+          this.model.clear();
+        }
+
+      });
+
+
+
+
+
+
+
+
+
+
+
+  var AppView = Backbone.View.extend({
+
+    el: $("#focalenderapp"),
+
+    events: {
+      "keypress #new-event":  "createOnEnter",
+    },
+
+    initialize: function() {
+      this.input = this.$("#new-event");
+
+      Events.bind('add', this.addOne, this);
+      Events.bind('reset', this.addAll, this);
+      Events.bind('all', this.render, this);
+
+      this.main = $('#main');
+
+      Events.fetch();
+    },
+
+    render: function() {
+      if (Events.length) {
+        this.main.show();
+      } else {
+        this.main.hide();
+      }
+    },
+
+    addOne: function(event) {
+      var view = new EventView({model: event});
+      this.$("#event-list").append(view.render().el);
+    },
+
+    addAll: function() {
+      Events.each(this.addOne);
+    },
+
+    createOnEnter: function(e) {
+      if (e.keyCode != 13) return;
+      if (!this.input.val()) return;
+
+      Events.create({title: this.input.val()});
+      this.input.val('');
+    },
+
+
+  });
+
+  var App = new AppView;
+
+});
+
