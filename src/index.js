0ea790a30e8c97b4399ac233da76695343b8addd
Jorin Vogel <jorin.vogel@gmail.com>
2013-09-22 17:07:27 +0200

    infinite scrolling

--- src/index.js
+++ src/index.js
@@ -1,63 +1,78 @@
 (function() {
 
+  var container = byClass('container');
+
+
   // main entry point for the application
   function main() {
 
-    console.log('focalendar is up and running!');
+    var date = new Date();
+    var month = createMonth(date);
+    container.appendChild(month);
+
+    window.onscroll = infiniteScroll;
+
+  }
 
 
-    //
-    // list days
 
-    var container = byClass('container');
+  // createDay(date) - returns a day DOM element
+  var createDay = (function() {
 
     var baseDay = byClass('day');
     baseDay.remove();
 
+    return function(date) {
+      var day = baseDay.cloneNode(true);
+      var label = byClass('day-label', day);
+
+      label.innerText = dayOfWeek(date) + ' ' + date.getDate();
+      day.setAttribute('data-date', date.toDateString());
+
+      // mark today
+      if (isToday(date)) day.id = 'today';
+
+      return day;
+    };
+
+  })();
+
+
+
+  // createMonth(date)
+  // returns a month DOM element for the month of the given date
+  var createMonth = (function() {
+
     var baseMonth = byClass('month');
     baseMonth.remove();
 
+    return function(date) {
 
-    var date = new Date();
-    month = createMonth(baseMonth, baseDay, date);
-    container.appendChild(month);
+      var month = baseMonth.cloneNode(true);
+      var header = byClass('month-header', month);
+      var days = byClass('days', month);
+      var monthNumber = date.getMonth();
 
-  }
+      // TODO: year for January is wrong when scrolling down
+      header.innerText = nameOfMonth(date) + ' ' + date.getUTCFullYear();
 
+      // start at first of month
+      date.setDate(1);
 
-  function createMonth(baseMonth, baseDay, date) {
+      while (date.getMonth() === monthNumber) {
+        var day = createDay(date);
+        days.appendChild(day);
+        date.setDate( date.getDate() + 1 );
+      }
 
-    var month = baseMonth.cloneNode(true);
-    var header = byClass('month-header', month);
-    var days = byClass('days', month);
+      return month;
+    };
 
-    var monthNumber = date.getMonth();
-
-    header.innerText = nameOfMonth(date) + ' ' + date.getUTCFullYear();
-
-    // start at first of month
-    date.setDate(1);
-
-    while (date.getMonth() === monthNumber) {
-      var day = createDay(baseDay, date);
-      days.appendChild(day);
-      date.setDate( date.getDate() + 1 );
-    }
-
-    return month;
-  }
+  })();
 
 
-  function createDay(base, date) {
-    var day = base.cloneNode(true);
-    var label = byClass('day-label', day);
-    label.innerText = dayOfWeek(date) + ' ' + date.getDate();
-    day.setAttribute('data-date', date.toDateString());
-    if (isToday(date)) day.id = 'today';
-    return day;
-  }
 
-
+  // returns a boolean
   function isToday(date) {
     return date.toDateString() === new Date().toDateString();
   }
@@ -69,23 +84,24 @@
   }
 
 
-  // returns a function that takes a date
-  // and returns something like 'JANURARY' or 'SEPTEMBER'
+
+  // nameOfMonth(date) - returns the name of the month
+  // example: 'JANURARY' or 'SEPTEMBER'
   var nameOfMonth = (function() {
 
     var monthNames = [
-      'Janurary',
-      'Februrary',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
+      'JANURARY',
+      'FEBRURARY',
+      'MARCH',
+      'APRIL',
+      'MAY',
+      'JUNE',
+      'JULY',
+      'AUGUST',
       'SEPTEMBER',
-      'October',
-      'November',
-      'December'
+      'OCTOBER',
+      'NOVEMBER',
+      'DECEMBER'
     ];
 
     return function(date) {
@@ -93,6 +109,38 @@
     };
 
   })();
+
+
+
+  function infiniteScroll() {
+    var top = scrollTop();
+    var maxTop = document.body.scrollHeight - document.documentElement.clientHeight;
+
+    if (top <= 0) return fetchEarlierDates();
+    if (top >= maxTop) fetchLaterDates();
+  }
+
+
+  function fetchEarlierDates() {
+    var firstDay = byClass('day').getAttribute('data-date');
+    var previousDay = new Date(firstDay);
+    previousDay.setDate( previousDay.getDate() - 1 );
+    var previousMonth = createMonth(previousDay);
+    var currentMonth = byClass('month');
+    container.insertBefore(previousMonth, currentMonth);
+    scrollTop(currentMonth.offsetTop);
+  }
+
+
+  function fetchLaterDates() {
+    var days = document.getElementsByClassName('day');
+    var lastDay = days[days.length -1].getAttribute('data-date');
+    var nextDay = new Date(lastDay);
+    nextDay.setDate( nextDay.getDate() + 1 );
+    var nextMonth = createMonth(nextDay);
+    container.appendChild(nextMonth);
+  }
+
 
 
   // get single element by classname
@@ -103,8 +151,22 @@
 
 
 
+  // cross-browser get or set scrollTop
+  function scrollTop(val) {
+    var doc = document.documentElement;
+    var body = document.body;
+
+    if (val) return doc.scrollTop = body.scrollTop = val;
+
+    return doc.scrollTop || body.scrollTop || 0;
+  }
+
+
+
   // kick of the application
   main();
+
+
 
   // ROADMAP
   //
@@ -113,5 +175,7 @@
   // make month headers sticky to the month they belong to
   // add a notifier pointing in the direction where today is
   // grey out past days
+
+
 
 })();
